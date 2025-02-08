5f16f394fea7b4cad9dd53d0e98b09020100062f
rprados5 <rprados5@alumno.uned.es>
2025-02-08 12:17:25 +0100

    Delete Scripts de R/FocosAnnioProv.R

--- Scripts de R/FocosAnnioProv.R
+++
@@ -1,7 +0,0 @@
-#Gráfico de focos al año
-#2016
-csvurl<-"https://github.com/rprados5/TFS_Matemamaticas/blob/main/CSVs%20Epidemiologicos/datosEp2016.csv?raw=true"
-ep2016p<-read.csv(url(csvurl),
-                 header = TRUE,sep = ";",encoding = "UTF-8")
-ba<-subset(ep2016p,Provincia== "BADAJOZ")
-cc<-subset(ep2016p,Provincia== "CÁCERES")
