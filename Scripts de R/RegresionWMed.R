46e981ed034339ec33aa749701e094dae6531f57
rprados5 <rprados5@alumno.uned.es>
2025-04-06 16:58:40 +0200

    Add files via upload

--- Scripts de R/RegresionWMed.R
+++ Scripts de R/RegresionWMed.R
@@ -2,12 +2,13 @@
 # VectorFocos.R
 # VectorTMax.R
 
+#Regresión simple
 regresion<-lm(focos~WMed)
 
 plot(WMed, focos)
 abline(regresion, col="red")
 
-
+#Test de normalidad
 qqnorm(regresion$residuals, pch = 19, col = "gray50", main="",xlab="Cuantiles Teóricos",ylab="Cuantiles WMed")
 qqline(regresion$residuals,col="red")
 
